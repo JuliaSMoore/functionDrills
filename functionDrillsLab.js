08715e973e4e32318818ce10c0cc6e92f5a1824a
Julia Moore <peony.the.pretty@gmail.com>
2022-12-05 18:27:04 -0600

    correction

--- functionDrillsLab.js
+++ functionDrillsLab.js
@@ -392,12 +392,13 @@ let sampleArray = [0,1,2,3,4,7,5,6,8,9]
 
 //CODE HERE
 
-let arrOfNums = function(arr = []) {
-  if (arr = arr.sort()) {
-    return true
-  } else {
-    return false
+let arrOfNums = function(arr) {
+  for (let i = 0; i < arr.length - 1; i++) {
+    if (arr[i] > arr[i + 1]) {
+      return false;
+    }
   }
+  return true
 }
 
 let arrayIsAscending = arrOfNums(sampleArray) 
